00f0a68b0892d9feb61ae62e525631fbd9e28759
skillsetdev <skillsdev3@gmail.com>
2024-05-18 01:13:54 +0200

    Create a header with buttons

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-const contactButton = document.querySelector("#contact");
-const lmButton = document.querySelector("#learn-more");
-contactButton.addEventListener("mouseover", function (e) {
-  e.target.style.backgroundColor = "transparent";
-  e.target.style.color = "#28584e";
+const transButtonArray = [];
+transButtonArray.push(
+  document.querySelector("#contact"),
+  document.querySelector("#learn-more"),
+  document.querySelector("#contacti"),
+  document.querySelector("#aboutme"),
+  document.querySelector("#uslugi"),
+  document.querySelector("#blog")
+);
+
+transButtonArray.forEach((button) => {
+  button.addEventListener("mouseover", mouseOverButton);
+  button.addEventListener("mouseout", mouseOutButton);
 });
-contactButton.addEventListener("mouseout", function (e) {
+
+function mouseOverButton(e) {
+  e.target.style.backgroundColor = "rgb(255, 250, 244)";
+  e.target.style.color = "#28584e";
+}
+function mouseOutButton(e) {
   e.target.style.backgroundColor = "#28584e";
   e.target.style.color = "rgb(255, 250, 244)";
-});
-lmButton.addEventListener("mouseover", function (e) {
-  e.target.style.backgroundColor = "transparent";
-  e.target.style.color = "#28584e";
-});
-lmButton.addEventListener("mouseout", function (e) {
-  e.target.style.backgroundColor = "#28584e";
-  e.target.style.color = "rgb(255, 250, 244)";
-});
+}
