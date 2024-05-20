31ecb3f10ace991d25f9175ad6d5d4dd0b88b310
skillsetdev <skillsdev3@gmail.com>
2024-05-21 00:23:44 +0200

    Create about me page and set up navigation

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const transButtonArray = [];
 transButtonArray.push(
-  document.querySelector("#contact"),
-  document.querySelector("#learn-more"),
-  document.querySelector("#contacti"),
-  document.querySelector("#aboutme"),
-  document.querySelector("#uslugi"),
-  document.querySelector("#blog")
+  ...document.querySelectorAll("#options"),
+  ...document.querySelectorAll("#priem"),
+  ...document.querySelectorAll("#aboutme"),
+  ...document.querySelectorAll("#uslugi"),
+  ...document.querySelectorAll("#contacti"),
+  ...document.querySelectorAll("#blog") //The spread operator (...) is used to take the elements out of the NodeLists returned by document.querySelectorAll and put them directly into transButtonArray
 );
 
 transButtonArray.forEach((button) => {
   button.addEventListener("mouseover", mouseOverButton);
   button.addEventListener("mouseout", mouseOutButton);
+  button.addEventListener("click", redirectToPage);
 });
 
 function mouseOverButton(e) {
@@ -20,4 +21,22 @@ function mouseOverButton(e) {
 function mouseOutButton(e) {
   e.target.style.backgroundColor = "#28584e";
   e.target.style.color = "rgb(255, 250, 244)";
+}
+function redirectToPage(e) {
+  switch (e.target.id) {
+    case "aboutme":
+      window.location.href = "about-me.html";
+      break;
+    case "uslugi":
+      window.location.href = "index.html";
+      break;
+    case "contacti":
+      window.location.href = "index.html";
+      break;
+    case "blog":
+      window.location.href = "index.html";
+      break;
+  }
+
+  mouseOutButton(e);
 }
